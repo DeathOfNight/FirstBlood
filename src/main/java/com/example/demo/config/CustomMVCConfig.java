dece450209830fc40846ad749d2a793a52b294c1
machenglong <machenglong@bestvike.com>
2017-09-29 14:31:07 +0800

    statics

--- src/main/java/com/example/demo/config/CustomMVCConfig.java
+++ src/main/java/com/example/demo/config/CustomMVCConfig.java
@@ -1,9 +1,15 @@
 package com.example.demo.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 public class CustomMVCConfig extends WebMvcConfigurerAdapter {
@@ -21,4 +27,23 @@ public class CustomMVCConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry){
         resourceHandlerRegistry.addResourceHandler("/**").addResourceLocations("/");
     }
+
+
+    /**
+     * 重写转换方法
+     * @author yimeidaoren77
+     */
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        super.configureMessageConverters(converters);
+////1.创建一个convert消息转换对象
+//        FastJsonHttpMessageConverter fastConvert = new FastJsonHttpMessageConverter();
+////2.创建一个fastJson的配置对象,然后配置格式化信息
+//        FastJsonConfig config = new FastJsonConfig();
+//        config.setSerializerFeatures(SerializerFeature.PrettyFormat);
+////3.在convert中添加配置信息
+//        fastConvert.setFastJsonConfig(config);
+////4.将convert添加到converts里面
+//        converters.add(fastConvert);
+//    }
 }
