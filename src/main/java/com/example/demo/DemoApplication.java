5a637d84f0cf121136791368981c9df58f3a79f9
zhangjun@bestvike.com <zhangjun@bestvike.com>
2017-09-29 10:17:22 +0800

    修改按钮弹出模态框model

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,8 @@
 package com.example.demo;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.converter.HttpMessageConverter;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.demo"})
